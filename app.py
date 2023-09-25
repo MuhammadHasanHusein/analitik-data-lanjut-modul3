3e67ae85b4e6c82a7bed9408ef5a702c98681758
unknown <sinnerhuman887@gmail.com>
2023-09-25 11:07:57 +0700

    upload all files

--- app.py
+++ app.py
@@ -1,34 +1,18 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import mysql.connector
 
-def get_connection():
-    connection =mysql.connector.connect(host='localhost', user='root', password='', database ='db_dal')
-    return connection
-
-def get_data_from_db ():
-    conn = get_connection()
-    query = "SELECT * FROM pddikti_example"
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
 #Judul Aplikasi
 st.title('Streamlit Simple App')
 
 #Menambahkan Navigasi di Sidebar
-page = st.sidebar.radio("Pilih halaman", ["Dataset", "Visualisasi", "Form Input"])
-
-
+page = st.sidebar.radio("Pilih halaman", ["Dataset", "Visualisasi"])
 
 if page == "Dataset":
     st.header("Halaman Dataset")
 
     #Baca File CSV
-    #data = pd.read_csv("pddikti_example.csv")
-
-    #Ubah Menjadi
-    data = get_data_from_db()
+    data = pd.read_csv("pddikti_example.csv")
 
     #Tampilkan data di streamlit
     st.write(data)
@@ -38,9 +22,7 @@ elif page == "Visualisasi":
     st.set_option('deprecation.showPyplotGlobalUse', False)
 
     #Baca file CSV
-    #data = pd.read_csv ("pddikti_example.csv")
-
-    data = get_data_from_db()
+    data = pd.read_csv ("pddikti_example.csv")
 
     #Filter berdasarkan universitas
     selected_university = st.selectbox('Pilih Universitas', data['universitas'].unique())
@@ -64,29 +46,3 @@ elif page == "Visualisasi":
     plt.legend()
 
     st.pyplot()
-
-elif page == "Form Input":
-    st.header("Halaman Form Input")
-    with st.form(key='input_form'):
-        input_semester = st.text_input('Semester')
-        input_jumlah = st.number_input ('Jumlah', min_value=0, format='%d')
-        input_program_studi = st.text_input('Program Studi')
-        input_universitas = st.text_input ('Universitas')
-        submit_button = st.form_submit_button(label='Submit Data')
-
-
-    if submit_button:
-        conn = get_connection()
-        cursor = conn.cursor ()
-        query = """
-        INSERT INTO pddikti_example(semester, jumlah, program_studi, universitas) VALUES (%s, %s, %s, %s) """
-        cursor.execute(query, (input_semester, input_jumlah, input_program_studi, input_universitas))
-        conn.commit()
-        conn.close()
-        st.success("Data Successfully submitted to the Database")
-    
-
-
-
-
-
